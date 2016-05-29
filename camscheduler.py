46f8445e8f425339e8e746400e228a50f4d0107a
Daniel Casner <daniel@anki.com>
2016-05-28 21:39:33 -0700

    Conceptually complete MQTT camera scheduler server.

--- camscheduler.py
+++ camscheduler.py
@@ -28,11 +28,12 @@ class SnapshotAction:
     """An class to store the necessary information to execute 1 or more snapshots
     immediately or at a time in the future."""
     
-    def __init__(self, foscam, preset, callback, number=1, interval=None, expire=None):
+    def __init__(self, foscam, preset, callback, number=1, interval=0.0, expire=None, userdata=None):
         """Setup the action closure.
         @param foscam driver object
         @param preset Preset to take snapshots at
         @param callback Function to call with image data
+               arguments will be (image data, final image, userdata)
         @param number Count of snapshots to take
         @param interval If number > 1, interval between snapshots
         @param expire If time passes expire, delete the task
@@ -43,6 +44,7 @@ class SnapshotAction:
         self.number   = number
         self.interval = interval
         self.expire   = expire
+        self.userdata = userdata
         self.nextTime = 0.0
         
     def run(self):
@@ -57,7 +59,7 @@ class SnapshotAction:
             self.cam.goto_preset(self.preset)
             time.sleep(SEEK_TIME)
             while self.number > 0:
-                self.callback(self.cam.snapshot())
+                self.callback(self.cam.snapshot(), self.preset, self.number==1, self.userdata)
                 self.number -= 1
                 if self.interval <= SEEK_TIME:
                     time.sleep(self.interval)
@@ -75,7 +77,7 @@ class FoscamScheduler(scheduler.Scheduler):
         scheduler.Scheduler.__init__(self)
         self.cam = foscam
     
-    def snapshot(self, priority, preset, callback, expire=None):
+    def snapshot(self, priority, preset, callback, expire=None, userdata=None):
         """Request a snapshot at a given preset.
         snapshots will go into the priority queue and execute when it is their
         turn. A snapshot will not be cancelled if another request with higher 
@@ -85,9 +87,9 @@ class FoscamScheduler(scheduler.Scheduler):
         @param callback Function to call with the photo data
         @param expire Latest time that the caller wants the photo. None for no expiration.
         """
-        self.append(priority, SnapshotAction(self.cam, preset, callback, expire=expire))
+        self.append(priority, SnapshotAction(self.cam, preset, callback, expire=expire, userdata=userdata))
     
-    def interval(self, priority, preset, callback, number, period, expire=None):
+    def interval(self, priority, preset, callback, number, period, expire=None, userdata=None):
         """Requests a series of snapshots at a given present.
         @param priority honor system priority number for this request, larger numbers = higher priority.
         @param preset The preset to take the picture at.
@@ -96,7 +98,7 @@ class FoscamScheduler(scheduler.Scheduler):
         @param period How many seconds between pictures.
         @param expire Latest time that the caller wants the photo. None for no expiration.
         """
-        self.append(priority, SnapshotAction(self.cam, preset, callback, number, period, expire))
+        self.append(priority, SnapshotAction(self.cam, preset, callback, number, period, expire, userdata))
         
     def queueDone(self):
         "Action when there are no more requests on the camera"
--- foscamqtt.py
+++ foscamqtt.py
@@ -4,3 +4,59 @@ An MQTT node that runs a camera scheduler for one or more foscams.
 The camera scheduler takes care of managing the use of the cameras
 """
 __author__ = "Daniel Casner <www.danielcasner.org>"
+
+import sys, os, json
+import camscheduler
+import paho.mqtt.client as mqtt
+
+class CamqttServer(mqtt.Client):
+    "ISA MQTT client, HAS several cam schedulers"
+
+    REQUEST_SUFFIX = "_request"
+
+    def on_snapshot(self, snapshot, preset, final, userdata):
+        self.publish(userdata, json.dumps({'preset': preset, 'final': final}).encode() + b'\0' + snapshot)
+
+    def on_connect(self, userdata, flags, rc):
+        "Subscribe to topics on connect in case of disconnection"
+        for camName in self.schedulers.keys():
+            self.subscribe(name + self.REQUEST_SUFFIX, qos=2)
+            
+    def on_message(self, userdata, msg):
+        if not msg.topic.endswith(self.REQUEST_SUFFIX):
+            sys.stderr.write("Received unexpected topic: \"{}\"{}".format(msg.topic, os.linesep))
+        else:
+            camera = msg.topic[:-len(self.REQUEST_SUFFIX)]
+            if not camera in self.schedulers:
+                sys.stderr.write("Received request for camera \"{}\" but we only have{linesep}\t{}{linesep}".format(camera, repr(self.schedulers.keys(), linesep=os.linesep)))
+            else:
+                try:
+                    command, args = json.loads(msg.payload)
+                    args.update({'callback': self.on_snapshot, 'userdata': cammera})
+                    if command == 'snapshot':
+                        self.schedulers[camera].snapshot(**args)
+                    elif command == 'internal':
+                        self.schedulers[camera].interval(**args)
+                    else:
+                        raise ValueError("Invalid scheduler command: " + command)
+                except Exception as e:
+                    sys.stderr.write(str(e))
+    
+    
+    def __init__(self, clientID, broker, cameras):
+        """Initalize camera server
+        @param clientID The MQTT client ID for the server
+        @param The host name / IP address of the broker
+        @param cameras A dictionary of named foscam instances. The names of the cameras be used as base names for MQTT
+        topics for requesting and receiving images. The server will subscribe to a topic <NAME>_request for each camera
+        to receive snapshot requests. Snapshots will be published as <NAME>_snapshot
+        """
+        mqtt.Client.__init__(self, clientID, True)
+        self.schedulers = {name: camscheduler.FoscamScheduler(camera) for name, camera in cameras.items()}
+        self.connect(broker)
+
+class CamqttClient(mqtt.Client):
+    "ISA MQTT client for requesting scheduled snapshots"
+    
+    def on_connect(self, userdata, flags, rc):
+        
